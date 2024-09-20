5a639630389caa957c212db3a1acf832b5386feb
DeepPK <108898993+DeepPK@users.noreply.github.com>
2024-09-20 14:25:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace hash_table
+{
+    public class Item
+    {
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+
+        public Item(string key, string value)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            Key = key;
+            Value = value;
+        }
+    }
+
+    public class HashTable
+    {
+        private readonly byte _maxSize = 255;
+        private Dictionary<string, List<Item>> _items = null;
+        public IReadOnlyCollection<KeyValuePair<string, List<Item>>> Items => _items?.ToList()?.AsReadOnly();
+        public HashTable()
+        {
+            _items = new Dictionary<string, List<Item>>(_maxSize);
+        }
+
+        public void Insert(string key, string value)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var item = new Item(key, value);
+            var hash = GetHash(item.Key);
+
+            List<Item> hashTableItem = null;
+            if (_items.ContainsKey(hash))
+            {
+                hashTableItem = _items[hash];
+
+                var oldElementWithKey = hashTableItem.SingleOrDefault(i => i.Key == item.Key);
+                if (oldElementWithKey != null)
+                {
+                    throw new ArgumentException($"{key} уже существуе, надо бы уникальный.", nameof(key));
+                }
+
+                _items[hash].Add(item);
+
+            }
+            else {
+                hashTableItem = new List<Item> { item };
+                _items.Add(hash, hashTableItem);
+            }
+        }
+        public void Delete(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            var hash = GetHash(key);
+
+            if (!_items.ContainsKey(hash))
+            {
+                return;
+            }
+
+            var hashTableItem = _items[hash];
+
+            var item = hashTableItem.SingleOrDefault(i => i.Key == key);
+            if (item != null)
+            {
+                hashTableItem.Remove(item);
+            }
+
+        }
+        public string Search(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            var hash = GetHash(key);
+
+            if (!_items.ContainsKey(hash))
+            {
+                return null;
+            }
+
+            var hashTableItem = _items[hash];
+
+            if (hashTableItem != null)
+            {
+                var item = hashTableItem.SingleOrDefault(i => i.Key == key);
+
+                if (item != null)
+                {
+                     return item.Value;
+                }
+            }
+             return null;
+        }
+        private string GetHash(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(value);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashTable = new HashTable();
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            using (var reader = new StreamReader(@"random_data.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    listA.Add(values[0]);
+                    listB.Add(values[1]);
+                }
+            }
+            for (int i = 0; i < listA.Count; i++) 
+            {
+                hashTable.Insert(listA[i], listB[i]);
+            }
+
+            ShowHashTable(hashTable, "================================Hashtable========================================");
+            Console.ReadLine();
+
+            hashTable.Delete("hsxbhfwisrawzfzyhbtanybvzywtvsqhbkmaxoqnswjzrofocwhwzcelyzljlikkqeelekguhcomgyziirmsqlwuuurrjdbwlhpq");
+            ShowHashTable(hashTable, "================================Deleted!=========================================================");
+            Console.ReadLine();
+
+            Console.WriteLine("ygtueguctywiwmleznjlztmntyjzxishwmtkjymyvmuyylkdhuqhglcorlccxoybixzszzahmmnrgiaehjuqbyjbwkqfdrfwxdnj");
+            var text = hashTable.Search("ygtueguctywiwmleznjlztmntyjzxishwmtkjymyvmuyylkdhuqhglcorlccxoybixzszzahmmnrgiaehjuqbyjbwkqfdrfwxdnj");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+        private static void ShowHashTable(HashTable hashTable, string title)
+        {
+            if (hashTable == null)
+            {
+                throw new ArgumentNullException(nameof(hashTable));
+            }
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+
+            Console.WriteLine(title);
+            int i = 0;
+            foreach (var item in hashTable.Items)
+            {
+                if (i < 25)
+                {
+                    Console.WriteLine(item.Key);
+
+                    foreach (var value in item.Value)
+                    {
+                        Console.WriteLine($"\t{value.Key} - {value.Value}");
+                    }
+                    i++;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
